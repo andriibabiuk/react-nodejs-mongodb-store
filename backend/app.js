4c8ab074d23f5993a282a9ae5985f96ffb8277dc
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-06-11 12:43:26 +0300

    feat: add database connection and functionality to save product in database

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
 
@@ -12,14 +11,13 @@ app.use(bodyParser.json());
 app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
-  // Set CORS headers so that the React SPA is able to communicate with this server
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+	);
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
 app.use('/products', productRoutes);
